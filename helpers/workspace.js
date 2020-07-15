632e703779dadb65926967a02b19d9a352da2f8e
LotuxPunk <vandendaelenclement@gmail.com>
2020-07-15 14:19:11 +0200

    createWorkspace return a Promise

--- helpers/workspace.js
+++ helpers/workspace.js
@@ -2,36 +2,35 @@ const fs = require('fs');
 const ncp = require('ncp').ncp;
 
 module.exports.createWorkspace = (uniqid, mapperId) => {
-    const folderPath = `./workspaces/${uniqid}/mapper-${mapperId}`;
-    ncp.limit = 16;
+    return new Promise((resolve, reject) => {
+        const folderPath = `./workspaces/${uniqid}/mapper-${mapperId}`;
+        ncp.limit = 16;
 
-    if (!fs.existsSync(folderPath)) {
-        fs.mkdirSync(folderPath);
+        if (!fs.existsSync(folderPath)) {
+            fs.mkdirSync(folderPath);
 
-        ncp(
-            './workspaces/DEFAULT',
-            folderPath,
-            {
-                filter: (path) => {
-                    return !(path.indexOf('mapper-image') > -1);
+            ncp(
+                './workspaces/DEFAULT',
+                folderPath,
+                {
+                    filter: (path) => {
+                        return !(path.indexOf('mapper-image') > -1);
+                    },
                 },
-            },
-            function (err) {
-                if (err) {
-                    return console.error(err);
-                }
-                console.log(`${folderPath} generated!`);
-            },
-        );
-    } else {
-        console.log('Directory already exists');
-    }
-    return folderPath;
+                function (err) {
+                    if (err) {
+                        reject(err);
+                    }
+                    resolve(folderPath);
+                },
+            );
+        } else {
+            reject(new Error('Directory already exists'));
+        }
+    });
 };
 
 module.exports.createEmptyWorkspace = (uniqid) => {
     const folderPath = `./workspaces/${uniqid}`;
     fs.mkdirSync(folderPath);
 };
-
-this.createWorkspace(123456);
