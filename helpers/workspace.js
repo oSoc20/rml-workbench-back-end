6d0b52f839344ffd910c86023c83d0aeb3f197d5
LotuxPunk <vandendaelenclement@gmail.com>
2020-07-15 11:43:45 +0200

    Workspace create mapper folder

--- helpers/workspace.js
+++ helpers/workspace.js
@@ -1,23 +1,28 @@
 const fs = require('fs');
 const ncp = require('ncp').ncp;
 
-module.exports.createWorkspace = (uniqid) => {
-    const folderPath = `./workspaces/${uniqid}`;
+module.exports.createWorkspace = (uniqid, mapperId) => {
+    const folderPath = `./workspaces/${uniqid}/mapper-${mapperId}`;
     ncp.limit = 16;
 
-    if(!fs.existsSync(folderPath)){
+    if (!fs.existsSync(folderPath)) {
         fs.mkdirSync(folderPath);
 
-        ncp('./workspaces/DEFAULT', folderPath, 
-            { filter: (path) => {
-                    return !(path.indexOf("mapper-image")> -1)
+        ncp(
+            './workspaces/DEFAULT',
+            folderPath,
+            {
+                filter: (path) => {
+                    return !(path.indexOf('mapper-image') > -1);
+                },
+            },
+            function (err) {
+                if (err) {
+                    return console.error(err);
                 }
-            }, function (err) {
-            if (err) {
-                return console.error(err);
-            }
-            console.log(`${folderPath} generated!`);
-        }); 
+                console.log(`${folderPath} generated!`);
+            },
+        );
     } else {
         console.log('Directory already exists');
     }
