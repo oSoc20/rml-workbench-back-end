ba94170ab56bfde15db9d597f9ad3a48daf93089
LotuxPunk <vandendaelenclement@gmail.com>
2020-07-16 10:40:59 +0200

    Zip helper use a Promise

--- helpers/zip.js
+++ helpers/zip.js
@@ -2,22 +2,25 @@ var file_system = require('fs');
 var archiver = require('archiver');
 
 module.exports.createZip = (uniqid) => {
-    const folderPath = `./workspaces/${uniqid}`;
+    return new Promise((resolve, reject) => {
+        const folderPath = `./workspaces/${uniqid}`;
 
-    var output = file_system.createWriteStream(`${folderPath}/workspace.zip`);
-    var archive = archiver('zip');
-    
-    output.on('close', function () {
-        console.log('archiver has been finalized and the output file descriptor has closed.');
+        var output = file_system.createWriteStream(`${folderPath}/workspace.zip`);
+        var archive = archiver('zip');
+
+        output.on('close', function () {
+            console.log('archiver has been finalized and the output file descriptor has closed.');
+            resolve();
+        });
+
+        archive.on('error', function (err) {
+            reject(err);
+        });
+
+        archive.pipe(output);
+
+        archive.directory(folderPath, false);
+        archive.directory('./workspaces/DEFAULT/mapper-image/', 'mapper-image/', false);
+        archive.finalize();
     });
-    
-    archive.on('error', function(err){
-        throw err;
-    });
-    
-    archive.pipe(output);
-    
-    archive.directory(folderPath, false);
-    archive.directory('./workspaces/DEFAULT/mapper-image/', 'mapper-image/', false);
-    archive.finalize();
-}
+};
