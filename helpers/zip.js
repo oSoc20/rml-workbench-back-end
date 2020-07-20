28b6cd44daff613cff912f4286b87628b3ba21fd
LotuxPunk <vandendaelenclement@gmail.com>
2020-07-20 15:59:00 +0200

    Added a zip method only for outputs

--- helpers/zip.js
+++ helpers/zip.js
@@ -14,7 +14,6 @@ module.exports.createZip = (uniqid) => {
         var archive = archiver('zip', { zlib: { level: 9 } });
 
         output.on('close', function () {
-            console.log('archiver has been finalized and the output file descriptor has closed.');
             resolve();
         });
 
@@ -26,6 +25,41 @@ module.exports.createZip = (uniqid) => {
         archive.pipe(output);
 
         archive.directory(folderPath, false);
+        //archive.directory('./workspaces/DEFAULT/mapper-image/', 'mapper-image/', false);
+        archive.finalize();
+    });
+};
+
+module.exports.createZipWithOutput = (uniqid, processorsLength) => {
+    return new Promise((resolve, reject) => {
+        const publicFolder = `./public/downloads/`;
+        const folderPath = `./workspaces/${uniqid}`;
+
+        // if (!fs.existsSync(folderPath)){
+        //     fs.mkdirSync(folderPath);
+        // }
+
+        var output = file_system.createWriteStream(`${publicFolder}/${uniqid}.zip`);
+        var archive = archiver('zip', { zlib: { level: 9 } });
+
+        output.on('close', function () {
+            resolve();
+        });
+
+        archive.on('error', function (err) {
+            console.error(err);
+            reject(err);
+        });
+
+        archive.pipe(output);
+
+        for (let index = 0; index < processorsLength; index++) {
+            archive.directory(
+                `${folderPath}/mapper-${index}/output`,
+                `output-mapper-${index}`,
+                false,
+            );
+        }
         //archive.directory('./workspaces/DEFAULT/mapper-image/', 'mapper-image/', false);
         archive.finalize();
     });
