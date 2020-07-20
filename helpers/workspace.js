19b07213ce6fecf105bf5ad756cee7b7d1020f7b
LotuxPunk <vandendaelenclement@gmail.com>
2020-07-20 15:59:24 +0200

    Added a deploy method

--- helpers/workspace.js
+++ helpers/workspace.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 const ncp = require('ncp').ncp;
+const b64helper = require('./base64');
+const dockerHelper = require('./dockerCompose');
 
 module.exports.createWorkspace = (uniqid, mapperId) => {
     return new Promise((resolve, reject) => {
@@ -33,4 +35,30 @@ module.exports.createWorkspace = (uniqid, mapperId) => {
 module.exports.createEmptyWorkspace = (uniqid) => {
     const folderPath = `./workspaces/${uniqid}`;
     fs.mkdirSync(folderPath);
+};
+
+module.exports.deployWorkspace = (processors, sources, token) => {
+    for (let index = 0; index < processors.length; index++) {
+        const processor = processors[index];
+
+        this.createWorkspace(token, index)
+            .then((mapperFolder) => {
+                //inputs
+                for (source of processor.sources) {
+                    b64helper.base64ToFile(sources[source], source, `${mapperFolder}/input`);
+                }
+
+                //mapper-config
+                b64helper.base64ToFile(
+                    processor.config,
+                    'mapping.rml.ttl',
+                    `${mapperFolder}/mapper-config`,
+                );
+
+                dockerHelper.editDC(token, processor.target, index);
+            })
+            .catch((err) => {
+                console.error(err);
+            });
+    }
 };
